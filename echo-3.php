adf04e772d6234a69671ba2ce8cc324e91dac083
18liangmei <49504830+18liangmei@users.noreply.github.com>
2019-05-09 09:46:05 +0800

    Add files via upload

---
+++ echo-3.php
@@ -0,0 +1,11 @@
+
+<?php
+$txt1="学习 PHP";
+$txt2="RUNOOB.COM";
+$cars=array("Volvo","BMW","Toyota");
+ 
+echo $txt1;
+echo "<br>";
+echo "在 $txt2 学习 PHP ";
+echo "<br>";
+echo "我的教室是 {$classroom[0]}";
