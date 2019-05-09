64eff3555b4667b6cdf6192d4957293947441744
18liangmei <49504830+18liangmei@users.noreply.github.com>
2019-05-09 10:01:30 +0800

    Add files via upload

--- shujuleixing-5.php
+++ shujuleixing-5.php
@@ -1,13 +1,13 @@
 
 <?php
 $x=5895;
-var_dump($x)
+var_dump($x);
 echo"<br>";
 $x=-345;//负数 
-var_dump"$x";
+var_dump($x);
 echo"<br>";
 $x=0x8C; // 十六进制数
-var_dump($x)；
+
 echo"<br>";
 $x=047; // 八进制数
 var_dump($x);
