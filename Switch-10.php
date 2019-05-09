05789759b93b1bb9050dc3da492917efa4a550b0
18liangmei <49504830+18liangmei@users.noreply.github.com>
2019-05-09 09:10:13 +0800

    Add files via upload

---
+++ Switch-10.php
@@ -0,0 +1,21 @@
+
+<?php
+$fuvcolor="smoking";
+switch ($favcolor)
+{
+case"smoking":
+    echo "I’m like smoking"
+    break;
+case"photo":
+    echo "I’m like photo"
+    break;
+case"passing":
+    echo "I’m like passing "
+    break;
+case"litteting":
+    echo "I’m like litteting"
+    break;		
+default:
+echo"I’m like noting litteting smoking,photo,passing,or litteting"
+}
+?>
