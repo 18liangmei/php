ac88b5198d68b66337f71ad6cf9165e497f38238
18liangmei <49504830+18liangmei@users.noreply.github.com>
2019-05-16 08:13:21 +0800

    Add files via upload

---
+++ php/26-1.php
@@ -0,0 +1,19 @@
+
+<HTML>
+<HEAD>
+<TITLE>简单的函数</TITLE>
+</HEAD>
+<BODY>
+<FONT>
+<?
+   function printBold($inpuText)
+   {
+        print("<B>" . $inputText . "</B>"); 
+	}
+	print("这行没有加重！<BR>\n"）；
+	PrintBold("这行加重了！！！");
+	print("<br>");
+	print9("这行没有加重！<BR>\n"); 	   
+?>
+<BODY>
+</HTML>
