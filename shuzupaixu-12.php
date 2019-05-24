9edfbc4bed62f73eb5622b4cdfc82066510f5bbd
18liangmei <49504830+18liangmei@users.noreply.github.com>
2019-05-24 09:37:37 +0800

    Add files via upload

---
+++ shuzupaixu-12.php
@@ -0,0 +1,15 @@
+
+<?php
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+arsort($age);
+?>
+
+<?php
+$numbers=array(4,6,2,22,11,);
+rsort($numbers);
+?>
+
+<?php
+$cars=arry("volvo","bmw","toyota");
+sort($cars);
+?>
